65787e42290d0d1cc8d7e5b5c04163a9211fbc80
Erik Iglikov <erik.iglikov@gmail.com>
2018-09-02 05:51:13 -0400

    music fade in, secondary elements fade in

--- src/js/index.js
+++ src/js/index.js
@@ -25,22 +25,10 @@ if (hours > 6 && hours < 21) {
   changeDayTime(nightCopy);
 }
 
-// function vidFade() {
-//   vid.classList.add('stopfade');
-// }
-
-// vid.addEventListener('ended', function() {
-//   // only functional if 'loop' is removed
-//   vid.pause();
-//   // to capture IE10
-//   vidFade();
-// });
-
 
 title.addEventListener('click', function() {
   changeDayTime(button.innerHTML);
 })
-
 button.addEventListener('click', function() {
   changeDayTime(button.innerHTML);
 })
@@ -49,19 +37,24 @@ function toggleLights() {
   vid.classList.toggle('stopfade');
   title.classList.toggle('button');
   button.classList.toggle('day-on');
+  night.classList.toggle('flicker');
 }
 
 function changeDayTime(daytime) {
   toggleLights();
   if (daytime == dayCopy) {
-    myAudio2.play();
-    myAudio4.pause();
-    night.classList.toggle('flicker')
+    // myAudio2.play();
+    // myAudio4.pause();
+    krusovice.tools.fadeOut(myAudio4, 2000);
+    krusovice.tools.fadeIn(myAudio2, 3000);
+
     button.innerHTML = nightCopy;
   } else {
-    myAudio2.pause();
-    myAudio4.play();
-    night.classList.toggle('flicker')
+    // myAudio2.pause();
+    // myAudio4.play();
+    krusovice.tools.fadeOut(myAudio2, 3000);
+    krusovice.tools.fadeIn(myAudio4, 2000);
+
     button.innerHTML = dayCopy;
   }
 }
@@ -69,7 +62,7 @@ function changeDayTime(daytime) {
 
 
 
-// time
+// clock
 function startTime() {
   var today = new Date();
   var h = today.getHours();
@@ -95,6 +88,9 @@ function checkTime(i) {
   return i;
 }
 
+
+
+// workaround for chrome to start autoplay
 function eventFire(el, etype){
   if (el.fireEvent) {
     el.fireEvent('on' + etype);
@@ -104,5 +100,25 @@ function eventFire(el, etype){
     el.dispatchEvent(evObj);
   }
 }
-
 eventFire(document.getElementById('clock'), 'click');
+
+
+// audio fade in/out
+function getSoundAndFadeAudio(soundObject) {
+
+  // Set the point in playback that fadeout begins. This is for a 2 second fade out.
+  var fadePoint = soundObject.duration - 2; 
+
+  var fadeAudio = setInterval(function () {
+
+      // Only fade if past the fade out point or not at zero already
+      if ((soundObject.currentTime >= fadePoint) && (soundObject.volume != 0.0)) {
+          soundObject.volume -= 0.1;
+      }
+      // When volume at zero stop all the intervalling
+      if (soundObject.volume === 0.0) {
+          clearInterval(fadeAudio);
+      }
+  }, 200);
+
+}
